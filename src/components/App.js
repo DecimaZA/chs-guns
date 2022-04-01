15b28f17ea4962de5901479f8fe8a75d753b5ee4
Jonathan Wright <devorakwright@gmail.com>
2022-04-01 22:55:56 +0200

    merge wip fix conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./ui/Header";
-<<<<<<< HEAD
+
 import Route from "./Route";
 import Accessories from "./core-components/Accessories";
 import Contact from "./core-components/Contact";
@@ -9,7 +9,7 @@ import Products from "./core-components/Products";
 import Services from "./core-components/Services";
 import FaQ from "./core-components/FaQ";
 import theme from './ui/Themes';
-=======
+
 //import Route from "./Link";
 import { BrowserRouter, Routes, Route, Switch, Link, Outlet } from "react-router-dom";
 // import Accessories from "./core-components/Accessories";
@@ -19,46 +19,11 @@ import { BrowserRouter, Routes, Route, Switch, Link, Outlet } from "react-router
 // import Services from "./core-components/Services";
 // import FaQ from "./core-components/FAQ";
 
->>>>>>> wip-navigation
 //MUI imports
 import Box from "@mui/material/Box";
 import { ThemeProvider } from '@mui/material/styles';
 
 const App = () => {
-<<<<<<< HEAD
-    return ( 
-        <ThemeProvider theme={theme}>    
-            <Box 
-                sx={{width: 1920,
-                    height: 1080,
-                    border: "10px",
-                    backgroundColor: "gray",
-                }}
-            >
-                <div>
-                    <Header />
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/products">
-                        <Products />
-                    </Route>
-                    <Route path="/accessories">
-                        <Accessories />
-                    </Route>
-                    <Route path="/services">
-                        <Services />
-                    </Route>
-                    <Route path="/contact">
-                        <Contact />
-                    </Route>
-                    <Route path="/faq">
-                        <FaQ />
-                    </Route>
-                </div>
-            </Box>
-        </ThemeProvider>  
-=======
     return (       
         <Box 
             sx={{width: 1920,
@@ -73,7 +38,6 @@ const App = () => {
             </div>
             <Outlet />
         </Box>
->>>>>>> wip-navigation
     );
 }
 
