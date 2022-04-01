89466e2a1a9a48fe419d3733d5f315add5af06fb
Jonathan Wright <devorakwright@gmail.com>
2022-04-01 22:59:57 +0200

    fix conflict in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,15 @@
 import React from "react";
 import Header from "./ui/Header";
-
-import Route from "./Route";
-import Accessories from "./core-components/Accessories";
-import Contact from "./core-components/Contact";
-import Home from "./core-components/Home";
-import Products from "./core-components/Products";
-import Services from "./core-components/Services";
-import FaQ from "./core-components/FaQ";
-import theme from './ui/Themes';
-
 //import Route from "./Link";
-import { BrowserRouter, Routes, Route, Switch, Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 // import Accessories from "./core-components/Accessories";
 // import Contact from "./core-components/Contact";
 // import Home from "./core-components/Home";
 // import Products from "./core-components/Products";
 // import Services from "./core-components/Services";
 // import FaQ from "./core-components/FAQ";
-
 //MUI imports
 import Box from "@mui/material/Box";
-import { ThemeProvider } from '@mui/material/styles';
 
 const App = () => {
     return (       
@@ -34,7 +22,6 @@ const App = () => {
         >
             <div>
                 <Header/>
-                
             </div>
             <Outlet />
         </Box>
