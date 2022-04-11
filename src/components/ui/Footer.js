a10bb86ad6c78df494842583da22591bdbdad272
Jonathan Wright <devorakwright@gmail.com>
2022-04-11 12:14:35 +0200

    Finished footer.

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -9,8 +9,6 @@ import { Box, Grid, Typography, Paper } from "@mui/material";
 
 const today = new Date();
 
-const pages = ["Home", "Products", "Accessories", "Services", "FAQ", "Contact"];
-
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
@@ -34,11 +32,11 @@ const Footer = () => {
         //                 </Grid>  
         //             </Grid>
         //             <Grid item xs={3} sm={3} md={6} padding={ "2rem" }>  
-        //                 <NavLink
-        //                     to={'/products'}                
-        //                 >                           
-        //                     PRODUCTS                           
-        //                 </NavLink>
+                        // <NavLink
+                        //     to={'/products'}                
+                        // >                           
+                        //     PRODUCTS                           
+                        // </NavLink>
         //             </Grid>
         //             <Grid item xs={3} sm={3} md={6} padding={ "2rem" }>  
         //                 <NavLink
@@ -53,36 +51,67 @@ const Footer = () => {
         // </Box>
 
         <Box sx={{ backgroundColor: "black", width: '100%' }}>
-            <Grid container direction="column">
-                <Grid container direction="column" rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 12 }} padding={2}>
-                    <Grid item xs={4}>
-                        <Item>g</Item>
-                    </Grid>
-                    <Grid container direction="column" columnSpacing={{ xs: 1, sm: 2, md: 12 }}>
-                        <Grid item xs={4}>
-                            <Item>Home</Item>
-                            <Item>Products</Item>
-                            <Item>Accessories</Item>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Item>g</Item>
-                    </Grid>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Grid item xs={1} md={3}>
+                    
                 </Grid>
-                <Grid container direction="column" rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 12 }} padding={2}>
-                    <Grid item xs={4} align="center">
-                        <Item>g</Item> 
-                    </Grid>
-                    <Grid container direction="column" columnSpacing={{ xs: 1, sm: 2, md: 6 }}>
-                        <Grid item xs={4}>
-                            <Item>Services</Item>
-                            <Item>FAQ</Item>
-                            <Item>Contact us</Item>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={4} align="center">
-                        <Item>g</Item> 
-                    </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/home'}>                           
+                        HOME                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/products'}>                           
+                        PRODUCTS                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/accessories'}>                           
+                        ACCESSORIES                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/services'}>                           
+                        SERVICES                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/faq'}>                           
+                        FAQ                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={12} sm={12} md={3} textAlign='center'>
+                    <NavLink to={'/contact'}>                           
+                        CONTACT US                           
+                    </NavLink>
+                </Grid>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>  
+            </Grid>
+            <Grid container rowSpacing={2}>
+                <Grid item xs={1} md={3}>
+                    
+                </Grid>
+                <Grid item xs={12} sm={10} md={12} textAlign='center'>
+                    <Typography color="white">
+                        COPYRIGHT CHS GUNS {today.getFullYear()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    
                 </Grid>
             </Grid>
         </Box>
