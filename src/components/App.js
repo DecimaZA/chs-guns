5a985adca7a7944a55df945d36cb7e40ec576d6b
Jonathan Wright <devorakwright@gmail.com>
2022-04-11 20:11:52 +0200

    Re-arranged contact page

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "./ui/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./ui/Footer";
+import brandCollage from "./images/logosFolder/brandCollage.png";
+import glockLogo from './images/logosFolder/glockLogo.png';
 //MUI imports
-import Box from "@mui/material/Box";
+import {Box, Grid} from "@mui/material/";
 
 const App = () => {
     return (       
         <Box backgroundColor="#cccccc" sx={{width: "100%", height: "100%"}} position="relative">
             <Header />            
-            <Outlet />
+            <Outlet /> 
             <Footer />
         </Box>
     );
--- src/components/core-components/Contact.js
+++ src/components/core-components/Contact.js
@@ -2,81 +2,96 @@ import React from "react";
 import MapsContainer from '../../apis/GoogleMaps';
 import theme from "../ui/Themes";
 //MUI imports
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import {Box, Container} from '@mui/material/';
 import { ThemeProvider, Typography } from "@mui/material";
 import { Grid } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const Contact = () => {
     return (
         <ThemeProvider theme={theme}>
-            <Typography component={'span'}>
-                <Grid container justify="center" spacing={10} marginTop="1px" >   
-                    <Grid item>
-                        <Grid container direction="column">
-                            <Grid item  >
-                                <Typography variant="h1" marginLeft={4}>
-                                    About our exact location...
-                                </Typography>
-                                    <Typography variant="h2" marginLeft={10}>
-                                        Street access and parking
-                                    </Typography>
-                                        <Typography align="left" variant="body1" marginLeft={4} marginTop={1} marginRight={130}>
-                                            Basement Level, The Beehive, 159 Main Road, Somerset West, 7130, Western Cape, South Africa, Earth, Milky Way Galaxy, the Universe
-                                            Our store location is tucked away behind The Beehive building, with vehicle access from both Main Road and St James Street (even
-                                            though Google Maps makes it appear there is a building in the way).
-                                        </Typography >
-                                        <Typography align="left" variant="body1" marginLeft={4} marginTop={1} marginRight={130}>    
-                                            If you are visiting us for the first time, the easiest access is from Main Road, through the narrow road/alley directly next 
-                                            to Ye Olde Bell. Keep left when you enter that way, and you will find yourself in a small parking area at the end, with another 
-                                            small road/alley at the end and on the left. You can use this road to exit on St James Street when you leave, and even use it to 
-                                            come see us the next time you visit! If you enter from St James Street, you will then of course be able to exit onto Main Road, 
-                                            or even back onto St James Street if you are brave enough or otherwise able to turn around in the narrow parking area. 
-                                        </Typography>
-                                        <Typography variant="h2" marginLeft={10}>
-                                            The store entrance...
-                                        </Typography>
-                                        <Typography align="left" variant="body1" marginLeft={4} marginTop={1} marginRight={130}>
-                                            Our store entrance is to the right of the steel fire escape, down some steps. You will see the thick steel barred gate that
-                                            gun shop customers are all to familiar with. Beyond that gate and immediately to the left is another smaller gate that directly
-                                            enters the store. If you walk past it, you will find yourself in the section of the CHS Bunker occupied by WildMedix, and maybe 
-                                            encounter a wild Gideon lurking and waiting to help you like the good CHS elf he is.
-                                        </Typography>
-                                        <Typography variant="h2" marginLeft={10}>
-                                            Our operating hours:
-                                        </Typography>
-                                        <Typography align="left" variant="body1" marginLeft={4} marginTop={1} marginRight={130}>
-                                            We all have to sleep and do admin sometime! Please note out operating hours:
-                                            Monday: Closed
-                                            Tuesday: 09:00 - 17:00
-                                            Wednesday: 09:00 - 17:00
-                                            Thuesday: 09:00 - 17:00
-                                            Friday: 09:00 - 17:00
-                                            Saturday: Closed
-                                            Sunday: Closed
-                                        </Typography>
-                                        <Typography variant="h3" marginLeft={10}>
-                                            Call or drop us an email for any questions or comments you may have!
-                                        </Typography>
-                                        <Typography align="left" variant="body1" marginLeft={4} marginTop={1} marginRight={130}>
-                                            Store contact number: 083 252 5097
-                                            Store email: aziza@chsguns.co.za
-                                        </Typography>
-                            </Grid>
-                        </Grid>
+            <Box sx={{ width: '100%' }}>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center" margin="40px">
+                    <Grid item xs={12} textAlign="center">
+                        <Typography variant="h1" align="center">
+                            About our exact location...
+                        </Typography>
                     </Grid>
-                    <Grid item>
-                        <Grid container direction="column" justify="center">
-                            <Grid item>
-                                <Container>
-                                    <MapsContainer />
-                                </Container>
-                            </Grid>
-                        </Grid>
+                    <Grid item xs={6} textAlign="center">
+                        <Typography variant="h2" align="center">
+                            Street access and parking
+                        </Typography>
                     </Grid>
-
+                    <Grid item xs={6} textAlign="center">
+                        <Typography align="center" variant="body1">
+                            Basement Level, The Beehive, 159 Main Road, Somerset West, 7130, Western Cape, South Africa, Earth, Milky Way Galaxy, the Universe
+                            Our store location is tucked away behind The Beehive building, with vehicle access from both Main Road and St James Street (even
+                            though Google Maps makes it appear there is a building in the way).
+                        </Typography >
+                        <Typography align="center" variant="body1">    
+                            If you are visiting us for the first time, the easiest access is from Main Road, through the narrow road/alley directly next 
+                            to Ye Olde Bell. Keep left when you enter that way, and you will find yourself in a small parking area at the end, with another 
+                            small road/alley at the end and on the left. You can use this road to exit on St James Street when you leave, and even use it to 
+                            come see us the next time you visit! If you enter from St James Street, you will then of course be able to exit onto Main Road, 
+                            or even back onto St James Street if you are brave enough or otherwise able to turn around in the narrow parking area. 
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} textAlign="center">
+                        <Typography variant="h2" align="center">
+                            The store entrance...
+                        </Typography>
+                    </Grid>
+                    <Grid item  xs={6} textAlign="center">
+                        <Typography align="center" variant="body1">
+                            Our store entrance is to the right of the steel fire escape, down some steps. You will see the thick steel barred gate that
+                            gun shop customers are all to familiar with. Beyond that gate and immediately to the left is another smaller gate that directly
+                            enters the store. If you walk past it, you will find yourself in the section of the CHS Bunker occupied by WildMedix, and maybe 
+                            encounter a wild Gideon lurking and waiting to help you like the good CHS elf he is.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} textAlign="center">
+                        <Typography variant="h2">
+                            Our operating hours:
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align="center" variant="body1">
+                            We all have to sleep and do admin sometime! Please note out operating hours:
+                        </Typography>
+                        <Typography align="center" variant="body1">
+                            Monday: Closed
+                            Tuesday: 09:00 - 17:00
+                            Wednesday: 09:00 - 17:00
+                            Thuesday: 09:00 - 17:00
+                            Friday: 09:00 - 17:00
+                            Saturday: Closed
+                            Sunday: Closed
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align="center" variant="h5">
+                            Call or drop us an email for any questions or comments you may have!
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography align="center" variant="body1">
+                            Store contact number: 083 252 5097
+                            Store email: aziza@chsguns.co.za
+                        </Typography>
+                    </Grid>
                 </Grid>
-            </Typography>
+            </Box>
+
+
         </ThemeProvider>
     );
 };
